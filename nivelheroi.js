79176481067ccf1dfbbef1fd2151d434e42aa395
alissoncavalcanti <alissoncavalcanti1@yahoo.com.br>
2025-07-19 11:02:09 -0300

    Primeiro commit

---
+++ nivelheroi.js
@@ -0,0 +1,15 @@
+let experienciaHeroi = 5320
+let eloHeroi
+let nomeHeroi = "Alisson"
+
+if (experienciaHeroi<1000){eloHeroi="Ferro"}
+else if (experienciaHeroi>=1001 && experienciaHeroi<2000) {eloHeroi="Bronze"}
+else if (experienciaHeroi>=2001 && experienciaHeroi<5000) {eloHeroi="Prata"}
+else if (experienciaHeroi>=5001 && experienciaHeroi<7000) {eloHeroi="Ouro"}
+else if (experienciaHeroi>=7001 && experienciaHeroi<8000) {eloHeroi="Platina"}
+else if (experienciaHeroi>=8001 && experienciaHeroi<9000) {eloHeroi="Ascendente"}
+else if (experienciaHeroi>=9001 && experienciaHeroi<10000) {eloHeroi="Imortal"}
+else if (experienciaHeroi >= 1001) {eloHeroi = "Radiante"}
+
+
+console.log ("O Herói de nome " + nomeHeroi + " está no nível " + eloHeroi)
